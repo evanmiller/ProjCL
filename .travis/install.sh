d3e4d5d9a0d1e8c343aa51232483975d7fdf6514
Evan Miller <emmiller@gmail.com>
2017-09-15 09:15:02 +0200

    Alien: fix warning

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,6 +5,5 @@ FILE=opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     curl -O http://registrationcenter-download.intel.com/akdlm/irc_nas/9019/${FILE}.tgz
     tar xzf ${FILE}.tgz
-    sudo alien ${FILE}/rpm/*.rpm
-    sudo dpkg -i *.deb
+    sudo alien --scripts -i ${FILE}/rpm/*.rpm
 fi
