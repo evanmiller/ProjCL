fdf688b35fc9c817f8ae593e665304ad80c9e9f5
Evan Miller <emmiller@gmail.com>
2017-09-15 09:04:09 +0200

    Maybe fix Linux

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,7 +5,6 @@ FILE=opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     curl -O http://registrationcenter-download.intel.com/akdlm/irc_nas/9019/${FILE}.tgz
     tar xzf ${FILE}.tgz
-    sudo apt-get install alien
-    sudo alien ${FILE}/rpm/*.rpm
+    alien ${FILE}/rpm/*.rpm
     sudo dpkg -i *.deb
 fi
