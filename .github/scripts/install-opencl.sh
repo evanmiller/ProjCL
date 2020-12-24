90513964287eb3a633ca2cdb14a7f8c389714765
Evan Miller <emmiller@gmail.com>
2020-12-24 09:45:37 -0500

    CI: Use Intel oneAPI for OpenCL installation
    The previous run-time was flakey, with "device unavailable" errors about
    50% of the time running on CI. The Intel oneAPI package seems to be
    much more reliable.

--- .github/scripts/install-opencl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -O http://registrationcenter-download.intel.com/akdlm/irc_nas/9019/${OPENCL_FILE}.tgz
-tar xzf ${OPENCL_FILE}.tgz
-sudo alien --scripts -i ${OPENCL_FILE}/rpm/*.rpm
