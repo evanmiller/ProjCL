954b022984d52ecc4beef724a63a176f2ecab658
Evan Miller <emmiller@gmail.com>
2020-12-24 10:36:07 -0500

    GH Actions: Install Proj.4 via package managers

--- .github/scripts/install-proj4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -O http://download.osgeo.org/proj/${PROJ4_FILE}.tar.gz
-tar xzf ${PROJ4_FILE}.tar.gz
-cd ${PROJ4_FILE}
-cmake . -DBUILD_GEOD=OFF -DBUILD_CCT=OFF -DBUILD_CS2CS=OFF -DBUILD_TESTING=OFF -DBUILD_GIE=OFF -DBUILD_PROJ=OFF -DBUILD_PROJINFO=OFF -DBUILD_PROJSYNC=OFF -DENABLE_CURL=OFF
-make
-sudo make install
-cd ..
